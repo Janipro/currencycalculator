ab7e36424f93fd6744bcafa8dcba1bab26d53567
Jan Adrian Hafskjold <jan.hafskjold@gmail.com>
2020-09-09 15:57:44 +0200

    Bug fix

--- src/currencycalculator/AppController.java
+++ src/currencycalculator/AppController.java
@@ -14,7 +14,7 @@ public class AppController {
     ComboBox<String> fromCurrency, toCurrency;
 
     @FXML
-    void initialize(){
+    void initialize() {
         var currencies = FXCollections.observableArrayList("USD", "NOK", "EUR");
         fromCurrency.setItems(currencies);
         toCurrency.setItems(currencies);
@@ -25,8 +25,8 @@ public class AppController {
         });
     }
 
-
-    void handleCurrencyInput(String value){
+    @FXML
+    void handleCurrencyInput(String value) {
         var value1 = fromCurrency.getValue();
         var value2 = toCurrency.getValue();
         var ratio = calculateRatio(value1, value2);
@@ -34,30 +34,31 @@ public class AppController {
 
         try {
             var currentCurrencyOutput = Double.parseDouble(currencyInput.getText()) * ratio;
-            currentCurrencyOutput = Math.abs(Math.round(currentCurrencyOutput*100.0))/100.0; //Instead of not allowing a negative input value (which can happen accidentally), this workaround takes care of the problem by using the absolute value.
+            currentCurrencyOutput = Math.abs(Math.round(currentCurrencyOutput * 100.0)) / 100.0; //Instead of not allowing a negative input value (which can happen accidentally), this workaround takes care of the problem by using the absolute value.
             currencyOutput.setText(Double.toString(currentCurrencyOutput));
         } catch (NumberFormatException e) {
             currencyOutput.setText("Please use numbers");
         }
     }
 
-    double calculateRatio(String currency1, String currency2){
+    @FXML
+    double calculateRatio(String currency1, String currency2) {
         var currency1Value = 0.0;
         var currency2Value = 0.0;
 
-        if (currency1.equals("USD")){
+        if (currency1.equals("USD")) {
             currency1Value = 8.85;
-        } else if (currency1.equals("NOK")){
+        } else if (currency1.equals("NOK")) {
             currency1Value = 1;
-        } else if (currency1.equals("EUR")){
+        } else if (currency1.equals("EUR")) {
             currency1Value = 10.53;
         }
 
-        if (currency2.equals("USD")){
+        if (currency2.equals("USD")) {
             currency2Value = 8.85;
-        } else if (currency2.equals("NOK")){
+        } else if (currency2.equals("NOK")) {
             currency2Value = 1;
-        } else if (currency2.equals("EUR")){
+        } else if (currency2.equals("EUR")) {
             currency2Value = 10.53;
         }
 
